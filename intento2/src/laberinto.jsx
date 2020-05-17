37f4f772fecbf229c67cc94b11f820c0fdbb3dbd
Abril Palencia <pal18198@uvg.edu.gt>
2020-05-17 13:09:42 -0600

    Movimiento del jugador

--- intento2/src/laberinto.jsx
+++ intento2/src/laberinto.jsx
@@ -11,6 +11,8 @@ const B = 10
 
 const Laberinto = () => {
     const [laberinto,setLaberinto]=React.useState([])
+    const [jugadorX, setjugadorX]=React.useState(1)
+    const [jugadorY, setjugadorY]=React.useState(1)
     useEffect(() => {
         fetch(`http://quetzaluno.space:3001/?w=${WIDTH}&h=${HEIGHT}&type=json`).then(
             (response) => {
@@ -24,6 +26,29 @@ const Laberinto = () => {
         
     },[])
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowRight') {
+          if (laberinto[jugadorY][jugadorX + 1] === ' ' || laberinto[jugadorY][jugadorX + 1] === 'p' || laberinto[jugadorY][jugadorX + 1] === 'g') {
+            setjugadorX(jugadorX + 1)
+          }
+        }
+        else if (e.key === 'ArrowLeft') {
+          if (laberinto[jugadorY][jugadorX - 1] === ' ' || laberinto[jugadorY][jugadorX - 1] === 'p' || laberinto[jugadorY][jugadorX - 1] === 'g') {
+            setjugadorX(jugadorX - 1)
+          }
+        }
+        else if (e.key === 'ArrowUp') {
+          if (laberinto[jugadorY - 1][jugadorX] === ' ' || laberinto[jugadorY - 1][jugadorX] === 'p' || laberinto[jugadorY - 1][jugadorX] === 'g') {
+            setjugadorY(jugadorY - 1)
+          }
+        }
+        else if (e.key === 'ArrowDown') {
+          if (laberinto[jugadorY + 1][jugadorX] === ' ' || laberinto[jugadorY + 1][jugadorX] === 'p' || laberinto[jugadorY + 1][jugadorX] === 'g') {
+            setjugadorY(jugadorY + 1)
+          }
+        }
+      }
+
     const style={
         marginTop: '100px',
         marginLeft: '310px',
@@ -35,7 +60,8 @@ const Laberinto = () => {
     } 
 
     return (
-    <div style={style}>
+    <div style={style} onKeyDown={handleKeyDown} tabIndex="0">
+        <Jugador x={jugadorX} y={jugadorY} />
         {laberinto.map((row,rowIndex)=>{
             return row.map((column,columnIndex)=>{
                 if(column === '+') {
@@ -44,12 +70,9 @@ const Laberinto = () => {
                     return <Pared2 key={`${rowIndex}-${columnIndex}`} x={columnIndex} y={rowIndex}/>
                 }else if(column === '|'){
                     return <Pared3 key={`${rowIndex}-${columnIndex}`} x={columnIndex} y={rowIndex}/>
-                }else if(column === 'p'){
-                    return <Jugador key={`${rowIndex}-${columnIndex}`} x={columnIndex} y={rowIndex}/>
                 }else if(column === 'g'){
                     return <Meta key={`${rowIndex}-${columnIndex}`} x={columnIndex} y={rowIndex}/>
-                }
-                else {
+                }else {
                     return null
                 }
                 
